b3f31e771c8b174060c6f6615d0564f3f16d2bf4
uhnette-2 <estraanet@gmail.com>
2025-11-21 00:22:47 -0800

    login/signin page

--- app/src/main/java/com/example/myapplication/Registration.kt
+++ app/src/main/java/com/example/myapplication/Registration.kt
@@ -47,7 +47,6 @@ class Registration : AppCompatActivity() {
             }
         }
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
